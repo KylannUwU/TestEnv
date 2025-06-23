52c76b20d5c883cddcc021d902aac6084822ad68
KylannUwU <kylannuwu@gmail.com>
2025-06-23 00:21:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -183,4 +183,7 @@ def get_call():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
